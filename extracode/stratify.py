7531b51a46ad2c38159b4be208570a18461063cf
Jezzamonn <jezzamonn@gmail.com>
2016-05-05 23:12:12 +1000

    Moving the stratify script to it's own folder

--- extracode/stratify.py
+++ extracode/stratify.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 headers = ['preg','glucos','bloodpresh','skinthickness','serum','bmi','dpf','age','class']
-df = pd.read_csv("pima.csv", header=None, names=headers, dtype='str')
+df = pd.read_csv("../pima.csv", header=None, names=headers, dtype='str')
 df_yes = df[df['class'] == 'yes']
 df_no = df[df['class'] == 'no']
 n_folds = 10
 
-with open('pima-folds.csv', 'w') as f:
+with open('../pima-folds.csv', 'w') as f:
     for i in range(n_folds):
         f.write('fold{}\n'.format(i+1))
         fold_yes = df_yes[i::n_folds]
